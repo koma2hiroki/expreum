7a790c0dc5591dfefd76a65459c2a7840fbc4105
haryu703 <34744512+haryu703@users.noreply.github.com>
2019-03-03 23:22:02 +0900

    add web3

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,13 @@
+extern crate web3;
+
+use web3::futures::Future;
+
 fn main() {
     println!("Hello, world!");
+
+    let (_eloop, transport) = web3::transports::Http::new("http://localhost:8545").unwrap();
+    let web3 = web3::Web3::new(transport);
+    let version = web3.web3().client_version().wait().unwrap();
+
+    println!("Client Version: {:?}", version);
 }
